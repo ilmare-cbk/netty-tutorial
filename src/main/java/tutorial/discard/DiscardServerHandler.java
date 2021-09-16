021873c7d230123a013f67402d57d022c70af8f2
cbk0315 <cbk0315@midasit.com>
2021-09-16 10:55:09 +0900

    refactor: Looking into the Received Data

--- src/main/java/tutorial/discard/DiscardServerHandler.java
+++ src/main/java/tutorial/discard/DiscardServerHandler.java
@@ -17,8 +17,20 @@ public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
      */
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        // Discard the received data silently.
-        ((ByteBuf) msg).release();
+//        // Discard the received data silently.
+//        ((ByteBuf) msg).release();
+
+        ByteBuf in = (ByteBuf) msg;
+
+        try {
+            while (in.isReadable()) {
+                System.out.println((char) in.readByte());
+                System.out.flush();
+            }
+        }
+        finally {
+            in.release();
+        }
     }
 
     @Override
