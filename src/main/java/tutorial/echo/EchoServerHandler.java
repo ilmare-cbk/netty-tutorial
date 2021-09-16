11677d3dbcc92bbef42cdcca35b52f167f0044c7
cbk0315 <cbk0315@midasit.com>
2021-09-16 11:11:29 +0900

    feat: Writing an Echo Server

---
+++ src/main/java/tutorial/echo/EchoServerHandler.java
@@ -0,0 +1,19 @@
+package tutorial.echo;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ctx.writeAndFlush(msg);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        // Close the connection when a exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
