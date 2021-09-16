fc30ab09420b06c07a1a907ceace7072b539a75a
cbk0315 <cbk0315@midasit.com>
2021-09-16 13:43:00 +0900

    refactor: Dealing with a Stream-based Transport
    - The First Solution

--- src/main/java/tutorial/time/TimeClientHandler.java
+++ src/main/java/tutorial/time/TimeClientHandler.java
@@ -8,17 +8,28 @@ import java.util.Date;
 
 public class TimeClientHandler extends ChannelInboundHandlerAdapter {
 
+    private ByteBuf buf;
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        buf = ctx.alloc().buffer(4);
+    }
+
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+        buf.release();
+        buf = null;
+    }
+
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         ByteBuf m = (ByteBuf) msg;
+        buf.writeBytes(m);
+        m.release();
 
-        try {
-            long currentTimeMills = (m.readUnsignedInt() - 2208988800L) * 1000L;
+        if (buf.readableBytes() >= 4) {
+            long currentTimeMills = (buf.readUnsignedInt() - 2208988800L) * 1000L;
             System.out.println(new Date(currentTimeMills));
-            ctx.close();
-        }
-        finally {
-            m.release();
         }
     }
 
